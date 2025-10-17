035bc7831001c7a97f2956bcd3e408e0dbb587d2
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-17 08:24:46 +0600

    add style in the btn

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -9,7 +9,7 @@ const Product = ({product}) => {
         <p>Phone : {phone} </p>
         <h2>Website : {website} </h2>
         <p>Email : {email} </p>
-        <button className='bg-emerald-600 mt-3'> Add to cart </button>
+        <button className='bg-emerald-600 p-4 mt-3 rounded-2xl cursor-pointer '> Add to cart </button>
     </div>
   )
 }
