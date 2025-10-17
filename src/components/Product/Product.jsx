d0f455aa4e40f911b8d5075379db890c7cc5c1d4
Mohammad Hasib Hasan <coder.mohammadhasibhasan@gmail.com>
2025-10-17 08:18:53 +0600

    add style in compo..

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const Product = ({product}) => {
- const {name, phone} =    product;
+ const {name, phone,website,email} =    product;
 
   return (
-    <div>
+    <div className='border-2 border-sky-800 m-2 py-8'>
         <h1>Name : {name} </h1>
+        <p>Phone : {phone} </p>
+        <h2>Website : {website} </h2>
+        <span>Email : {email} </span>
     </div>
   )
 }
